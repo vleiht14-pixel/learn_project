ceb6b28207e738aef394a8e2d52faa7d2e01449d
Лейхт Владислав <@>
2026-04-27 10:58:42 +0700

    Доделано полностью №2

---
+++ backend/schemas/item_schema.py
@@ -0,0 +1,11 @@
+from apiflask import Schema
+from apiflask.fields import Integer, String, List, Nested
+
+
+class ItemSchema(Schema):
+    id = Integer()
+    name = String()
+
+
+class ItemOutSchema(Schema):
+        items = List(Nested(ItemSchema))
